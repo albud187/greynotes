00c344559528c86ceace7ded3357f300637e47cb
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-10-24 15:12:20 +0000

    update models

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Article
 # Register your models here.
+from .models import TextNote, NoteGroup
 
 admin.site.register(Article)
+admin.site.register(TextNote)
+admin.site.register(NoteGroup)
--- backend/articles/models.py
+++ backend/articles/models.py
@@ -9,19 +9,26 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+class NoteGroup(models.Model):
+    group_name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.group_name
+
+class TextNote(models.Model):
+    note_group = models.ForeignKey(NoteGroup, null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length = 120)
+    archived = models.BooleanField(default=False)
+    date_created = models.DateField(auto_now=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return (str(self.date_created) +'_'+ self.title)
+
 # class NoteGroup(models.model):
 #     name = models.CharField(max_length =120)
 #
-# class TextNote(models.Model):
-#     grouping = models.ForeignKey(NoteGroup)
-#     title = models.CharField(max_length = 120)
-#     content = models.TextField()
-#     archived = models.BooleanField(default=False)
-#     date_created = 'models.datefield'
-#
-#     def __str__(self):
-#         return self.title
-#
+
 # class ListNote(models.Model):
 #     grouping = models.ForeignKey(NoteGroup)
 #     title = models.CharField(max_length = 120)
