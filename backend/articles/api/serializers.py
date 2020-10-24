ea4f0a53bd38b65b9b64d555aff6fbe902118f73
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-10-24 20:02:13 +0000

    serializers

--- backend/articles/api/serializers.py
+++ backend/articles/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from articles.models import Article
+from articles.models import Article, TextNote, NoteGroup
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('id','title', 'content')
+
+class NoteGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoteGroup
+        fields = ('group_name')
+
+class TextNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TextNote
+        fields =('id', 'title', 'content', 'note_group')
