d808ff4a92cede40ce1fecc62baa6dc0d7bc7d14
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-10-24 14:57:59 +0000

    update

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -8,3 +8,26 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+# class NoteGroup(models.model):
+#     name = models.CharField(max_length =120)
+#
+# class TextNote(models.Model):
+#     grouping = models.ForeignKey(NoteGroup)
+#     title = models.CharField(max_length = 120)
+#     content = models.TextField()
+#     archived = models.BooleanField(default=False)
+#     date_created = 'models.datefield'
+#
+#     def __str__(self):
+#         return self.title
+#
+# class ListNote(models.Model):
+#     grouping = models.ForeignKey(NoteGroup)
+#     title = models.CharField(max_length = 120)
+#     content = models.TextField()
+#     archived = models.BooleanField(default=False)
+#     date_created = 'models.datefield'
+#
+#     def __str__(self):
+#         return self.title
