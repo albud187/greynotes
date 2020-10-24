59a59b208859708fe8aa49f1c6589864b4070ff1
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-10-24 15:27:18 +0000

    update

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -38,3 +38,61 @@ class TextNote(models.Model):
 #
 #     def __str__(self):
 #         return self.title
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.template.defaultfilters import slugify
+from datetime import datetime
+
+# Create your models here.
+
+class NoteGroup(models.Model):
+    group_name = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return(self.group_name)
+
+# class BaseNote(models.Model):
+#     slug = models.SlugField(blank=True, null=True)
+#     date_created =models.DateTimeField(auto_now_add=True)
+#     parent_group = models.ForeignKey(NoteGroup, blank=True, null=True, on_delete=models.SET_NULL)
+#     title = models.CharField(max_length=200)
+#     archived = models.BooleanField(default=False)
+#
+#     def __str__(self):
+#         return(self.title)
+#
+#     def save(self, *args, **kwargs):
+#         original_slug = slugify(self.title)
+#         queryset = BaseNote.objects.all().filter(slug__iexact=original_slug).count()
+#
+#         count = 1
+#         slug = original_slug
+#         while(queryset):
+#             slug = original_slug + '-' + str(count)
+#             count += 1
+#             queryset = BaseNote.objects.all().filter(slug__iexact=slug).count()
+#
+#         self.slug = slug
+#
+#         # if self.featured:
+#         #     try:
+#         #         temp = BaseNote.objects.get(featured=True)
+#         #         if self != temp:
+#         #             temp.featured = False
+#         #             temp.save()
+#         #     except BaseNote.DoesNotExist:
+#         #         pass
+#         super(BaseNote, self).save(*args, **kwargs)
+#
+# class TextNote(BaseNote):
+#     note_content = models.TextField(null=True)
+#
+# class ListNote(BaseNote):
+#     pass
+#
+# class ListNoteEntry(models.Model):
+#     parent_list = models.ForeignKey(ListNote, on_delete=models.CASCADE)
+#     entry_text = models.TextField(null=True)
+#     completed = models.BooleanField(default=False)
