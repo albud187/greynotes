4d8f94c711ec4c4027d1676868ab96758ed3a43a
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-10-27 23:07:29 +0000

    run greynotes shortcut

--- run_greynotes.py
+++ run_greynotes.py
@@ -1,3 +1,23 @@
 import os
+import subprocess
 
-os.system("start /wait cmd {C:\Users\alfab\anaconda3\Scripts\activate.bat}")
+backend_commands = [
+    r"C:\Users\alfab\anaconda3\Scripts\activate.bat",
+    "activate djangoEnv001",
+    r'cd C:\Users\alfab\Documents\_Projects\greynotes\backend',
+    "python manage.py runserver"
+]
+
+frontend_commands = [
+    r"C:\Users\alfab\anaconda3\Scripts\activate.bat",
+    "activate djangoEnv001",
+    r'cd C:\Users\alfab\Documents\_Projects\greynotes\frontend',
+    'npm start'
+]
+
+def command_string(command_list):
+    cmd_string = ' && '.join(command_list)
+    return(cmd_string)
+
+backend = subprocess.Popen(["start", "cmd", "/k", command_string(backend_commands)], shell = True)
+frontend = subprocess.Popen(["start", "cmd", "/k", command_string(frontend_commands)], shell = True)
