229f4b47aa49f6465a2ffdd61d52cb2a6a9d539b
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-11-22 13:50:56 -0400

    update

--- frontend/apiEndPoints.js
+++
@@ -1,9 +0,0 @@
-export const API_LIST_NOTE_LIST =
-export const API_LIST_NOTE =
-export const API_LIST_NOTE_ENTRY_ALL =
-export const API_LIST_NOTE_ENTRY_LIST =
-
-export const API_TEXT_NOTE_LIST =
-export const API_TEXT_NOTE =
-export const API_NOTE_GROUP =
-export const API_MEME_TEXT = 
